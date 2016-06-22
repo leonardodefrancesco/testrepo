d628fe29965ace703052a61f738b23555d5fb336
leonardodefrancesco <leonardo@albergodefrancesco.it>
2016-06-22 23:35:33 +0200

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,52 @@
+citazione = 'terenzio disse: "la fortuna aiuta gli audaci"'
+print('es 1')
+print(citazione.title())
+
+
+
+my_name = 'leonardo'
+print('es 2')
+print(my_name.title(),' ', my_name.upper(),' ', my_name.lower())
+
+
+
+
+first_name = 'leonardo'
+last_name = 'defrancesco'
+
+print('es 3')
+
+full_name = first_name.title() + ' ' + last_name.title()
+print(full_name) 
+
+
+
+
+print('es 4')
+
+first_name = 'michele'
+last_name = 'calvanese'
+
+
+full_name = first_name.title() + ' ' + last_name.title()
+
+print('io ammiro', full_name)
+print('scherzo')
+
+
+print('es 5')
+
+name = '        leonardo        '
+
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+
+
+
+
+
+
+
